147b2773cb111da8622d99a108eb426a241ca2e0
Petr <vitaliy.tolkachev.2000@mail.ru>
2021-05-13 18:02:22 +0300

    bitrix start

--- index.php
+++ index.php
@@ -1,67 +1,30 @@
-<?php
-
-$first_number = $_POST['first'];
-$second_number = $_POST['second'];
-$operation = $_POST['operations'];
-
-$result = null;
-
-if (is_numeric($first_number) && is_numeric($second_number)) {
-    switch ($operation) {
-        case '+':
-            $result = $first_number + $second_number;
-            break;
-        case '-':
-            $result = $first_number - $second_number;
-            break;
-        case '*':
-            $result = $first_number * $second_number;
-            break;
-        case '/':
-            if ($second_number == 0) {
-                $result = 'На ноль делить нельзя';
-            } else {
-                $result = number_format($first_number / $second_number, 2);
-            }
-            break;
-        default:
-            $result = '';
-            break;
-    }
-} else {
-    $result = 'Введите число';
-}
-
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<form name="calculator" method="post" action='<?php $_SERVER['DOCUMENT_ROOT'] ?>'>
-    <label for="first">
-        Введите первое число: <input type="text" name="first" id="first" value="<?=$first_number?>" maxlength="10">
-    </label>
-    <label for="second">
-        Введите второе число: <input type="text" name="second" id="second" value="<?=$second_number?>" maxlength="10">
-    </label>
-    <br><br>
-    <span>Выберите операцию:</span>
-    <select name="operations" id="calc">
-        <option>+</option>
-        <option>-</option>
-        <option>*</option>
-        <option>/</option>
-    </select>
-    <br><br>
-    <input type="submit" value="Посчитать">
-    <?php if ($result !== null) { ?><p>Результат: <?=$result?></p><?php } ?>
-</form>
-</body>
-</html>
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Мебельная компания");
+?><p>
+Наша компания существует на Российском рынке с 1992 года. За это время «Мебельная компания» прошла большой путь от маленькой торговой фирмы до одного из крупнейших производителей корпусной мебели в России.
+</p><p>
+«Мебельная компания» осуществляет производство мебели на высококлассном оборудовании с применением минимальной доли ручного труда, что позволяет обеспечить высокое качество нашей продукции. Налажен производственный процесс как массового и индивидуального характера, что с одной стороны позволяет обеспечить постоянную номенклатуру изделий и индивидуальный подход – с другой.
+<h3>Наша продукция</h3>
+<?$APPLICATION->IncludeComponent("bitrix:furniture.catalog.index", "", array(
+	"IBLOCK_TYPE" => "products",
+	"IBLOCK_ID" => "2",
+	"IBLOCK_BINDING" => "section",
+	"CACHE_TYPE" => "A",
+	"CACHE_TIME" => "36000000",
+	"CACHE_GROUPS" => "N"
+	),
+	false
+);?>
+<h3>Наши услуги</h3>
+<?$APPLICATION->IncludeComponent("bitrix:furniture.catalog.index", "", array(
+	"IBLOCK_TYPE" => "products",
+	"IBLOCK_ID" => "3",
+	"IBLOCK_BINDING" => "element",
+	"CACHE_TYPE" => "A",
+	"CACHE_TIME" => "36000000",
+	"CACHE_GROUPS" => "N"
+	),
+	false
+);?>
+</p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
